911a9facc910dfafbaab0ace3fe3e84009a0b590
ahsan <makabir98@yahoo.com>
2022-02-11 21:16:07 +0600

    deposit, withdraw, update synced

--- js/banking.js
+++ js/banking.js
@@ -6,8 +6,34 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     console.log(depositAmount);
 
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+    depositTotal.innerText = parseFloat(depositTotal.innerText) + parseFloat(depositAmount);
+    console.log('Total deposit = ', depositTotal.innerText)
 
     //clear deposit input field
     depositedInput.value = '';
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    balanceTotal.innerText = parseFloat(balanceTotal.innerText)+parseFloat(depositAmount);
+    console.log('New Balance = ', balanceTotal.innerText)
+});
+
+//handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    //get the amount deposited
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+    console.log(withdrawAmount);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawAmount);
+    console.log('Total withdraw = ', withdrawTotal.innerText)
+
+    //clear withdraw input field
+    withdrawInput.value = '';
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    balanceTotal.innerText = parseFloat(balanceTotal.innerText)-parseFloat(withdrawAmount);
+    console.log('New Balance = ', balanceTotal.innerText)
 });
